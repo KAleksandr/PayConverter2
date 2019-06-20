5263fdf0e37ae933ede2739d8c424611a412964c
Oleksandr Kuzmenko <okuzmenkoa@gmail.com>
2019-06-20 09:35:49 +0300

    fix bag export excel

--- ExportToExcel.cs
+++ ExportToExcel.cs
@@ -31,14 +31,14 @@ namespace SoftGenConverter
                 int cellColumnIndex = 1;
 
                
-                for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+                for (int i = 0; i < dataGridView1.Rows.Count ; i++)
                 {
                     for (int j = 0; j < dataGridView1.Columns.Count; j++)
                     {
 
                         // Excel index starts from 1,1. As first Row would have the Column headers, adding a condition check.
-                        if (cellRowIndex == 1) worksheet.Cells[cellRowIndex, cellColumnIndex] = dataGridView1.Columns[j].HeaderText;
-                        else
+                        //if (cellRowIndex == 1) worksheet.Cells[cellRowIndex, cellColumnIndex] = dataGridView1.Columns[j].HeaderText;
+                       // else
                         {
                             //if (cellColumnIndex == 6)
                             //{
