fd2ba3ae7ca83ca13fe0f522058c2e1de4beb574
Oleksandr Kuzmenko <okuzmenkoa@gmail.com>
2019-06-20 11:11:04 +0300

    add save chenge line

--- Form1.cs
+++ Form1.cs
@@ -682,10 +682,25 @@ namespace SoftGenConverter
             dataGridView1.Visible = true;
             gridHeader.Text = label9.Text;
         }
-
+        //запис в data.xml призначення платежу
         private void DataGridView2_CellBeginEdit(object sender, DataGridViewCellCancelEventArgs e)
         {
+            int selRowNum = dataGridView2.SelectedCells[0].RowIndex;
+            int selColNum = dataGridView2.SelectedCells[0].ColumnIndex;
+            if (dataGridView2[e.ColumnIndex, e.RowIndex].Value != null)
+                if (selColNum == 11)
+                {
+                    DialogResult dialogResult = MessageBox.Show("Зміни записати базу данних", "Запис данних", MessageBoxButtons.YesNo);
+                    if (dialogResult == DialogResult.Yes)
+                    {
+                        int n = dataGridView1.Rows.Add();
+                        dataGridView3.Rows[n].Cells[0].Value = dataGridView2.Rows[selRowNum].Cells[selColNum - 1].Value; // 
+                        dataGridView3.Rows[n].Cells[1].Value = dataGridView2.Rows[selRowNum].Cells[selColNum + 1].Value; // 
+                        dataGridView3.Rows[n].Cells[2].Value = dataGridView2.Rows[selRowNum].Cells[selColNum].Value; // 
+                        Xml.saveXml(dataGridView3, path2);
+                    }
 
+                }
         }
     }
 }
